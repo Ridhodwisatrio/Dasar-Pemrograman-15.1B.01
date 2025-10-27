92c5ec6517362d1549ea09b846142a9be35d04aa
Ridho Dwi Satrio <ridhodwisatrio21@gmail.com>
2025-10-28 00:35:49 +0700

    Melanjutkan Latihan

--- Pertemuan 5/lat2.py
+++ Pertemuan 5/lat2.py
@@ -1,46 +1,60 @@
 # Layar Masukan
-print("GEROBAK FRIED CHICKEN")
-print(35*"-")
-print("Kode\t","Jenis potong\t","Harga")
-print(35*"-")
-print("D\t", "Dada\t", "\tRp. 2500")
-print("P\t", "Paha\t", "\tRp. 2000")
-print("S\t", "Sayap\t", "\tRp. 1500")
-print(35*"-","\n")
+print(''' \nGEROBAK FRIED CHICKEN
+--------------------------------
+Kode  Jenis Potong  Harga
+D     Dada          Rp.2500
+P     Paha          Rp.2000
+S     Sayap         Rp.1500
+--------------------------------
+''')
+
+# inisialisai data list
+jenis_potong = []
+harga_satuan = []
+banyak_beli = []
+total_semua = []
 
 # input
 banyak_jenis = int(input("Banyak jenis: "))
 for i in range(banyak_jenis):
-  print(f"Jenis ke- {str(i+1)}")
+  print(f"\nJenis ke- {str(i+1)}")
   kode_potong = input("Masukan kode potong [D/P/S]: ")
   banyak_potong = int(input("Masukan banyak potong: "))
-b = 0
+  if kode_potong.upper() == "D":
+    jenis = "Dada"
+    harga = 2500
+  elif kode_potong.upper() == "P":
+    jenis = "Paha"
+    harga = 2000
+  elif kode_potong.upper() == "S":
+    jenis = "Sayap"
+    harga = 1500
+  else:
+    jenis = "Maaf jenis potong tidak tersedia"
+    harga = 0
+  total = harga * banyak_potong
+# tempat buat nyimpen list
+  jenis_potong.append(jenis)
+  harga_satuan.append(harga)
+  banyak_beli.append(banyak_potong)
+  total_semua.append(total)
 
-# proses if
-if kode_potong.upper() == "D":
-  jenis_potong = "Dada"
-  harga = 2500
-elif kode_potong.upper() == "P":
-  jenis_potong = "Paha"
-  harga = 2000
-elif kode_potong.upper() == "S":
-  jenis_potong = "Sayap"
-  harga = 1500
-else:
-  jenis_potong = "Maaf jenis potong tidak tersedia"
-  harga = 0
-jenis_jenis = ['Dada', 'Paha', 'Sayap']
 # output
-print("\tGEROBAK FRIED CHICKEN")
-print(35*"-")
-print('''NO. Jenis\t Harga\t Banyak\t Jumlah
-    Potong\t Satuan\t Beli\t Harga''')
-print(35*"-")
-for i in range(len(jenis_jenis)):
-  print(b,".",jenis_jenis[i],"\t", harga,"\t", banyak_potong,"\t", "Rp.", (banyak_potong * harga))
-print(35*"-")
-print(f"Jumlah Bayar: Rp.{harga + harga}" )
-print(f"Pajak 10%: Rp.{harga * 0.1}")
+print('''\nGEROBAK FRIED CHICKEN
+-----------------------------------------
+No. Jenis     Harga     Banyak     Jumlah
+    Potong    Satuan    Beli       Harga
+-----------------------------------------
+''')
+jumlah_harga = 0
+for i in range(banyak_jenis):
+  print(i+1,".",jenis_potong[i],"    ",harga_satuan[i],"\t", banyak_beli[i],"\t", "  Rp.",total_semua[i])
+  jumlah_harga += total_semua[i]
+pajak = jumlah_harga * 0.1
+print(41*"-")
+print(f"Jumlah Bayar: Rp.{jumlah_harga}" )
+print(f"Pajak 10%: Rp.{pajak}")
+print(f"Total Bayar: Rp.{jumlah_harga + pajak}")
 
 
 
